9e4bffa3aa3c7c62fd39be395f577613d9e3c294
apepsiii <apepsiii@apepsiiis-MBP.lan>
2024-12-23 15:31:28 +0700

    new

--- index-ws.js
+++ index-ws.js
@@ -2,50 +2,39 @@ const express = require('express');
 const server = require('http').createServer();
 const app = express();
 
-// Menyajikan file index.html
 app.get('/', function(req, res) {
-    res.sendFile('index.html', { root: __dirname });
-});
+    res.sendFile('index.html', {root: __dirname});
 
-// Menghubungkan Express dengan HTTP server
+})
+
 server.on('request', app);
-server.listen(3000, function() {
-    console.log('Server started on port 3000');
-});
+server.listen(3000, function() {console.log('Server started on port 3000');});
 
-// Menginisialisasi WebSocket
+// Begin websocket
 const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({
     server: server
 });
 
-// Ketika klien terkoneksi ke WebSocket server
-wss.on('connection', function(ws) {
-    // Menampilkan jumlah klien yang terhubung
+wss.on('connection', function(wss) {
     const numClients = wss.clients.size;
-    console.log('Clients connected:', numClients);
+    console.log('Clients connected', numClients);
 
-    // Mengirimkan informasi jumlah klien terkoneksi ke semua klien
-    wss.broadcast(`Current visitors: ${numClients}`);
+    wss.broacast(`Current visitors: ${numClients} `);
 
-    // Menyambut klien yang terkoneksi
-    if (ws.readyState === ws.OPEN) {
+    if (ws.readState === ws.OPEN) {
         ws.send('Welcome to my server');
     }
 
-    // Menangani penutupan koneksi
-    ws.on('close', function() {
-        const numClients = wss.clients.size;  // Memperbarui jumlah klien setelah klien disconnect
+    ws.on('close', function close() {
         wss.broadcast(`Current Visitors: ${numClients}`);
-        console.log('A Client has disconnected');
-    });
-});
+        console.log('A Client has dissconnected');
+    })
 
-// Fungsi broadcast untuk mengirim pesan ke semua klien
-wss.broadcast = function(data) {
-    wss.clients.forEach(function(client) {
-        if (client.readyState === client.OPEN) {
-            client.send(data); // Mengirim pesan ke klien yang terhubung
-        }
+})
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data);
     });
-};
+}
